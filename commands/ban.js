54ed5e92d6a4f6aff1b4ebae509b365841e355fa
Myst <34489470+Myst82015@users.noreply.github.com>
2019-06-28 22:17:45 +0200

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,44 @@
+/**
+ * @param {import('discord.js').Client} client 
+ * @param {import('discord.js').Message} message 
+ * @param {String} args 
+ * @param {import('discord.js')} Discord
+ */
+
+// eslint-disable-next-line no-unused-vars
+async function run(client, message, args, Discord) {
+
+ if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You do not have permission to perform this command!")
+
+   let banMember = message.mentions.members.first() || message.guild.members.get(args[0]) 
+   if(!banMember) return message.channel.send("Please provide a user to ban!")
+
+   let reason = args.slice(1).join(" ");
+   if(!reason) reason = "No reason given!"
+
+   if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I dont have permission to perform this command")
+
+   banMember.send(`Hello, you have been banned from ${message.guild.name} for: ${reason}`).then(() =>
+   message.guild.ban(banMember, { days: 1, reason: reason})).catch(err => console.log(err))
+
+   message.channel.send(`**${banMember.user.tag}** has been banned`).then(m => m.delete(5000))
+
+    let embed = new RichEmbed()
+    .setColor(redlight)
+    .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
+    .addField("Moderation:", "ban")
+    .addField("Banned user:", banMember.user.username)
+    .addField("Moderator:", message.author.username)
+    .addField("Reason:", reason)
+    .addField("Date:", message.createdAt.toLocaleString())
+    
+     message.channel.send(embed)
+}
+    
+module.exports = {
+    config: {
+        name: 'ban', 
+        aliases: ["b]
+    },
+    run: run
+};
